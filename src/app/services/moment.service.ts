9ea093dc4f0a9d947a6e6ad014fbdeb98b80e453
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 19:33:54 -0300

    moment service

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Moment } from '../Moments';
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`
 
   constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
