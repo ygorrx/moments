de1292189508f54242010d7a0a2d35452bf5d0d0
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 18:03:10 -0300

    moment service form logic

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Moment } from '../Moments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
 
-  constructor() { }
+  private baseApiUrl = environment.baseApiUrl
+  private apiUrl = `${this.baseApiUrl}api/moments`
+
+  constructor(private http: HttpClient) { }
+
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
 }
