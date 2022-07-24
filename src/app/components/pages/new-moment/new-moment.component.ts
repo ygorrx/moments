1d9eece1073c53ce92bd4b291048c2651c838e14
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 15:46:48 -0300

    new moment component

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit {
+
+  btnText = "Compartilhar"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
