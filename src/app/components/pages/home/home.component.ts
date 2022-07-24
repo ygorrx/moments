714e90aeeefced91a14ddb5ae21e8281483aef07
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 19:34:39 -0300

    home component logic + style

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MomentService } from 'src/app/services/moment.service';
+
+import { Moment } from 'src/app/Moments';
+
+import { environment } from 'src/environments/environment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  allMoments: Moment[] = []
+  moments: Moment[] = []
+  baseApiUrl = environment.baseApiUrl
+  faSearch = faSearch
+  searchTerm: string = ''
+
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR')
+      })
+
+      this.allMoments = data
+      this.moments = data
+    })
+  }
+
+  search(event:Event):void{
+    const target = event.target as HTMLInputElement
+    const value = target.value
+    
+    this.moments = this.allMoments.filter(moment => {
+      return moment.title.toLowerCase().includes(value)
+    })
   }
 
 }
