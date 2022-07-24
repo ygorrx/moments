a0eee968135f519db507c25089b75ae2b85cf589
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 19:34:23 -0300

    adicionar momento

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moments';
+import { MessagesService } from 'src/app/services/messages.service';
 import { MomentService } from 'src/app/services/moment.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -11,7 +12,7 @@ export class NewMomentComponent implements OnInit {
 
   btnText = "Compartilhar"
 
-  constructor(private momentService:MomentService) { }
+  constructor(private momentService:MomentService, private messageService:MessagesService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -26,6 +27,10 @@ export class NewMomentComponent implements OnInit {
     }
 
     await this.momentService.createMoment(formData).subscribe()
+
+    this.messageService.add('Momento adicionado com sucesso!')
+
+    this.router.navigate(['/'])
 
   }
 
