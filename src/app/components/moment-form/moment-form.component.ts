557ce2598890e9b665a2db8be521dc68410509b7
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 21:09:23 -0300

    moment form component

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -10,16 +10,16 @@ import { Moment } from 'src/app/Moments';
 export class MomentFormComponent implements OnInit {
   @Output() onSubmit = new EventEmitter<Moment>()
   @Input() btnText!:string
-
+  @Input() momentData: Moment | null = null
   momentForm!: FormGroup
-
+  
   constructor() { }
 
   ngOnInit(): void {
     this.momentForm = new FormGroup({
-      id: new FormControl(''),
-      title: new FormControl('', [Validators.required]),
-      description: new FormControl('', [Validators.required]),
+      id: new FormControl(this.momentData ? this.momentData.id : ''),
+      title: new FormControl(this.momentData ? this.momentData.title : '', [Validators.required]),
+      description: new FormControl(this.momentData ? this.momentData.description : '', [Validators.required]),
       image: new FormControl('')
     })
   }
