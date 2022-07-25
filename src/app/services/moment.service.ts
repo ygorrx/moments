57eb74c1a7fd38799bfccf83233762f6ba34e2b5
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 21:08:19 -0300

    moment service

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -19,8 +19,23 @@ export class MomentService {
     return this.http.get<Response<Moment[]>>(this.apiUrl)
   }
 
+  getMoment(id: number): Observable<Response<Moment>>{
+    const url = `${this.apiUrl}/${id}`
+    return this.http.get<Response<Moment>>(url)
+  }
+
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  removeMoment(id: number){
+    const url = `${this.apiUrl}/${id}`
+    return this.http.delete(url)
+  }
+
+  updateMoment(id: number, formData: FormData): Observable<FormData> {
+    const url = `${this.apiUrl}/${id}`
+    return this.http.put<FormData>(url, formData)
   }
 
 }
