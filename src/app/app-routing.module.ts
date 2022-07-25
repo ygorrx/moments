d92e77e43ca498439ce83b37af09502292e7f4df
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-24 21:08:45 -0300

    including route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
+import { EditMomentComponent } from './componentes/page/edit-moment/edit-moment.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'about', component:AboutComponent},
-  {path: 'moments/new', component:NewMomentComponent}
+  {path: 'moments/new', component:NewMomentComponent},
+  {path: 'moments/edit/:id', component:EditMomentComponent},
+  {path: 'moments/:id', component:MomentComponent}
 ];
 
 @NgModule({
